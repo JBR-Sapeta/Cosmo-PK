d8d735d73a4309db753dd847f053a57f2a0dbfea
JBR-Sapeta <jakub.ryszard.sapeta@gmail.com>
2023-11-06 19:24:49 +0100

    fix: F-8 fix folder structure

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { type ReactElement } from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './Header/Hedader';
+import Header from './Header/Header';
 import Main from './Main/Main';
 import Footer from './Footer/Footer';
 
